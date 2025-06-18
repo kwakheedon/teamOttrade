c6ba41ce524c90b8bda32edfcd510c8536bb9d69
JG <wprkf1005@gmail.com>
2025-06-18 17:47:43 +0900

    백엔드 초기 세팅

---
+++ BackEnd/src/main/java/com/ottrade/ottrade/OttradeApplication.java
@@ -0,0 +1,13 @@
+package com.ottrade.ottrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttradeApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/ottrade/ottrade/OttradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ottrade.ottrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
