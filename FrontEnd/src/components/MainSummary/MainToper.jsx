81608167c77761d7e45e34d52fe8b2451885b05d
SeongjaeHong <auraghd@gmail.com>
2025-06-26 16:46:38 +0900

    feat: server 통신 추가

--- FrontEnd/src/components/MainSummary/MainToper.jsx
+++ FrontEnd/src/components/MainSummary/MainToper.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Main.css'
+import axios from 'axios'
+import SearchResultBox from './SearchResultBox'
 
 const MainToper = ({ onScrollClick }) => {
+
+    const [searchItem, setSearchItem] = useState("")
+    const [hsList, setHsList] = useState([])
+
+    const startSearch = async () => {
+        //const path = `http://192.168.219.86:8088/api/search-summary/${encodeURIComponent(searchItem)}`
+        const path = `/api/search-summary/${searchItem}`
+        try {
+            const {data} = await axios.get(path)
+            console.log(data)
+            setHsList(data.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
         <div className='main-box'>
             <h1 className="main-text">
@@ -9,10 +27,15 @@ const MainToper = ({ onScrollClick }) => {
                 수출 전략이 완성됩니다
             </h1>
 
-            <input className="input" type="text" />
+            <input
+                className="input"
+                type="text"
+                value={searchItem}
+                onChange={e => setSearchItem(e.target.value)}
+            />
             <p className='input-line'></p>
-
-            <button className='iconBtn'></button>
+            <SearchResultBox hsList={hsList}/>
+            <button className='iconBtn' onClick={startSearch}></button>
 
             {/* 스크롤 버튼 */}
             <button className='scrollBtn' onClick={onScrollClick}></button>
--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  server: {
+    proxy: {
+      '/api': 'http://192.168.219.86:8088'
+    }
+  }
 })
