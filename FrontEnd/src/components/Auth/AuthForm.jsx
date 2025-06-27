f754236bef3fcb411a8316868ebfd29aa2b2deee
서명우 <leo576004@gmail.com>
2025-06-27 17:59:23 +0900

    smw

--- FrontEnd/src/components/Auth/AuthForm.jsx
+++ FrontEnd/src/components/Auth/AuthForm.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router'
 import SocialLogin from './SocialLogin'
+import './AuthForm.css'
 
 // 로그인, 회원가입 폼
 const AuthForm = () => {
   return (
     <div>
       {/* 로그인 모달 창 끄는 맨 오른쪽 위 선형 버튼 */}
-      <button></button>
+      <button className='button'></button>
 
-      <h1>로그인</h1>
+      <h1 className='h1'>로그인</h1>
 
-      <input type="text" placeholder='전화번호'/>
-      <input type="text" placeholder='비밀번호'/>
-      <input type="submit" value="로그인" />
+      <div className="inputBox">
+        <div className="fields">
+          <input type="text" placeholder="전화번호" />
+          <input type="text" placeholder="비밀번호" />
+        </div>
+        <input type="submit" value="로그인" />
+      </div>
 
       {/* 회원가입 페이지로 이동 */}
       <Link to="/signup">새 계정 만들기↗</Link>
--- FrontEnd/src/components/Auth/SocialLogin.jsx
+++ FrontEnd/src/components/Auth/SocialLogin.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './AuthForm.css'
 
 // 소셜 로그인버튼을 저장할 컴포넌트
 const SocialLogin = () => {
   return (
-    <div>
+    <div className='SocialLogin'>
       <button>구글</button>
       <button>네이버</button>
       <button>카카오톡</button>
