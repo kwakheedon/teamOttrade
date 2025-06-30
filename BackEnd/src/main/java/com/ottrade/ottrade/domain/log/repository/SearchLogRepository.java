af05313e0fafbf46f04cb5157b909e56e9ec651b
제갈태웅 <wprkf1005@gmail.com>
2025-06-30 23:30:08 +0900

    feat : 사용자 ID와 키워드로 로그를 찾는 메소드 추가

--- BackEnd/src/main/java/com/ottrade/ottrade/domain/log/repository/SearchLogRepository.java
+++ BackEnd/src/main/java/com/ottrade/ottrade/domain/log/repository/SearchLogRepository.java
@@ -4,6 +4,11 @@ import com.ottrade.ottrade.domain.log.entity.SearchLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional; // Optional 임포트
+
 @Repository
 public interface SearchLogRepository extends JpaRepository<SearchLog, Long> {
+
+    // 사용자 ID와 키워드로 로그를 찾는 메소드 추가
+    Optional<SearchLog> findByUserIdAndKeyword(Long userId, String keyword);
 }
