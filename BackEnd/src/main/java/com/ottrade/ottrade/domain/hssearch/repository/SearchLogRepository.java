adb8d50c8c83068b6162187c5f95db5c8cba816d
제갈태웅 <wprkf1005@gmail.com>
2025-06-30 23:03:01 +0900

    chore : 리포지토리 이동

--- BackEnd/src/main/java/com/ottrade/ottrade/domain/hssearch/repository/SearchLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ottrade.ottrade.domain.hssearch.repository;
-
-import com.ottrade.ottrade.domain.hssearch.entity.SearchLogEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@org.springframework.stereotype.Repository
-public interface SearchLogRepository extends JpaRepository<SearchLogEntity, Long> {
-}
