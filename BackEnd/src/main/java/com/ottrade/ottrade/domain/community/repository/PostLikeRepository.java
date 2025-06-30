18b95165b54961dca25ef778eafef3174640f5fb
곽희돈 <khdon0405789@gmail.com>
2025-06-30 14:31:49 +0900

    코드병합

--- BackEnd/src/main/java/com/ottrade/ottrade/domain/community/repository/PostLikeRepository.java
+++ BackEnd/src/main/java/com/ottrade/ottrade/domain/community/repository/PostLikeRepository.java
@@ -3,11 +3,12 @@ package com.ottrade.ottrade.domain.community.repository;
 import com.ottrade.ottrade.domain.community.entity.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.ottrade.ottrade.domain.community.entity.PostLikeId;
 
 import java.util.Optional;
 
 @Repository
-public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
+public interface PostLikeRepository extends JpaRepository<PostLike, PostLikeId> {
     // 'boardId'가 아닌 'post'의 'id'를 참조하도록 메서드 이름 변경
     long countByPostId(Long postId);
 
