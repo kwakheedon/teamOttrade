6e721838736b4e049a45ea61a0810c5eccade18a
JG <wprkf1005@gmail.com>
2025-07-02 10:19:33 +0900

    feat : 가장많이 검색된 품목출력을 위한 Repositorty 추가

--- BackEnd/src/main/java/com/ottrade/ottrade/domain/log/repository/PnmLogRepository.java
+++ BackEnd/src/main/java/com/ottrade/ottrade/domain/log/repository/PnmLogRepository.java
@@ -1,9 +1,24 @@
 package com.ottrade.ottrade.domain.log.repository;
 
+import com.ottrade.ottrade.domain.log.dto.TopSearchKeywordDTO;
 import com.ottrade.ottrade.domain.log.entity.PnmLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 @Repository
 public interface PnmLogRepository extends JpaRepository<PnmLog, Long> {
+    /**
+     * 가장 많이 검색된 품목명(pnm) Top 10을 조회하는 쿼리
+     * @param pageable Top 10을 지정하기 위한 페이징 정보
+     * @return Top 10 검색어와 횟수가 담긴 DTO 리스트
+     */
+    @Query("SELECT new com.ottrade.ottrade.domain.log.dto.TopSearchKeywordDTO(p.pnm, COUNT(p.pnm)) " +
+            "FROM PnmLog p " +
+            "GROUP BY p.pnm " +
+            "ORDER BY COUNT(p.pnm) DESC")
+    List<TopSearchKeywordDTO> findTopKeywords(Pageable pageable);
 }
