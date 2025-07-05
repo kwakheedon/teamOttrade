141aba623ca7e2da2fa75299c28db3719bc184a0
JG <wprkf1005@gmail.com>
2025-07-05 13:04:08 +0900

    style : 메인 스크롤바 애니메이션 추가

--- FrontEnd/src/pages/public/MainPage.jsx
+++ FrontEnd/src/pages/public/MainPage.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import './MainPage.css'
+import './MainPage.css' //
 import MainToper from '../../components/MainSummary/MainToper'
 import TotalMembersBox from '../../components/MainSummary/TotalMembersBox'
 import TotalPostsBox from '../../components/MainSummary/TotalPostsBox'
@@ -16,6 +16,13 @@ const MainPage = () => {
       <section className="section-top">
         <MainToper onScrollClick={handleScrollClick} />
       </section>
+
+      {/* --- scrollBtn 내부를 채워줍니다 --- */}
+      <button className='scrollBtn' onClick={handleScrollClick}>
+        <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19 9l-7 7-7-7" stroke="#3b3b3b" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </button>
 
       <section className="section-bottom" ref={scrollTargetRef}>
         <TotalMembersBox />
