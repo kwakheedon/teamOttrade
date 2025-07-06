9f708f5c4d5016633fadf1d0966b6f6c5b78696d
제갈태웅 <wprkf1005@gmail.com>
2025-07-06 20:30:22 +0900

    refactor : WebConfig 수정

--- BackEnd/src/main/java/com/ottrade/ottrade/config/WebConfig.java
+++ BackEnd/src/main/java/com/ottrade/ottrade/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173") // 프론트엔드 서버 주소 허용
+                .allowedOrigins("*") // 프론트엔드 서버 주소 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키 등 자격 증명 허용
